506e149072cd5b6f4e12d65680cc48fa1f4f9484
Dharani122935 <143819830+Dharani122935@users.noreply.github.com>
2026-07-09 18:01:00 +0530

    File updated at 09/07/2026, 18:00:57

--- 38-CountandSay/38-CountandSay.java
+++ 38-CountandSay/38-CountandSay.java
@@ -1,27 +1,21 @@
-// Last updated: 09/07/2026, 18:00:05
+// Last updated: 09/07/2026, 18:00:57
 1class Solution {
-2    public int[][] modifiedMatrix(int[][] matrix) {
-3        int r = matrix.length;
-4        int c = matrix[0].length;
-5
-6        for (int j = 0; j < c; j++) {
-7            int max = Integer.MIN_VALUE;
-8
-9            // 1. Find max in column j
-10            for (int i = 0; i < r; i++) {
-11                if (matrix[i][j] != -1) {
-12                    max = Math.max(max, matrix[i][j]);
-13                }
-14            }
-15
-16            // 2. Replace all -1 with max
-17            for (int i = 0; i < r; i++) {
-18                if (matrix[i][j] == -1) {
-19                    matrix[i][j] = max;
-20                }
-21            }
-22        }
-23
-24        return matrix;
-25    }
-26}
+2    public int areaOfMaxDiagonal(int[][] dimensions) {
+3        double maxi = -1.0;
+4        int res = 0;
+5        
+6        for (int[] rect : dimensions) {
+7            int l = rect[0], b = rect[1];
+8            double d = Math.sqrt((double)l * l + (double)b * b);
+9            
+10            if (d > maxi) {
+11                maxi = d;
+12                res = l * b;
+13            } else if (d == maxi) {
+14                res = Math.max(res, l * b);
+15            }
+16        }
+17        
+18        return res;
+19    }
+20}
