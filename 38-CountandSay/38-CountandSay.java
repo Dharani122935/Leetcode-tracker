b683481e49abc7b07005048ba48e6fc167f2eeb1
Dharani122935 <143819830+Dharani122935@users.noreply.github.com>
2026-07-09 18:19:05 +0530

    File updated at 09/07/2026, 18:19:02

--- 38-CountandSay/38-CountandSay.java
+++ 38-CountandSay/38-CountandSay.java
@@ -1,24 +1,15 @@
-// Last updated: 09/07/2026, 18:16:22
-1import java.util.*;
-2class Solution {
-3    public int maxFrequencyElements(int[] nums) {
-4        int[] freq = new int[101];  // numbers are in range 1–100
-5        for (int num : nums) {
-6            freq[num]++;
-7        }
-8
-9        int maxFreq = 0;
-10        for (int f : freq) {
-11            maxFreq = Math.max(maxFreq, f);
-12        }
-13
-14        int result = 0;
-15        for (int f : freq) {
-16            if (f == maxFreq) {
-17                result += f;
-18            }
-19        }
-20
-21        return result;
-22    }
-23}
+// Last updated: 09/07/2026, 18:19:02
+1import java.util.HashSet;
+2
+3public class Solution {
+4    public boolean checkIfExist(int[] arr) {
+5        HashSet<Integer> set = new HashSet<>();
+6        for (int num : arr) {
+7            if (set.contains(num * 2) || (num % 2 == 0 && set.contains(num / 2))) {
+8                return true;
+9            }
+10            set.add(num);
+11        }
+12        return false;
+13    }
+14}
