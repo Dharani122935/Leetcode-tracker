70b70a82a85dbc11fa9f1799eb4a22f95ce21d0a
Dharani122935 <143819830+Dharani122935@users.noreply.github.com>
2026-07-09 18:11:49 +0530

    File updated at 09/07/2026, 18:11:45

--- 38-CountandSay/38-CountandSay.java
+++ 38-CountandSay/38-CountandSay.java
@@ -1,15 +1,15 @@
-// Last updated: 09/07/2026, 18:11:00
+// Last updated: 09/07/2026, 18:11:45
 1class Solution {
-2    public int findContentChildren(int[] g, int[] s) {
-3        Arrays.sort(g);
-4        Arrays.sort(s);
-5        int n = g.length, m = s.length, i = 0, j = 0;
-6
-7        while (i < m && j < n) {
-8            if (g[j] <= s[i]) j++;  
-9            i++;  
-10        }
-11
-12        return j;
+2    public List<Integer> findDisappearedNumbers(int[] nums) {
+3        List<Integer> result = new ArrayList<>();
+4		boolean[] n = new boolean[nums.length+1];
+5		for(int num : nums) {
+6			n[num] = true;
+7		}
+8		
+9		for(int i=1; i<=nums.length; i++) {
+10			if(!n[i]) result.add(i);
+11		}
+12        return result;
 13    }
 14}
