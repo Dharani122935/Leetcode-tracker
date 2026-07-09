6c4558a654e73d9d6a3287f20285f4acf5492b5d
Dharani122935 <143819830+Dharani122935@users.noreply.github.com>
2026-07-09 18:08:04 +0530

    File updated at 09/07/2026, 18:08:01

--- 38-CountandSay/38-CountandSay.java
+++ 38-CountandSay/38-CountandSay.java
@@ -1,14 +1,20 @@
-// Last updated: 09/07/2026, 18:03:29
+// Last updated: 09/07/2026, 18:08:01
 1class Solution {
-2    public int[] sortArrayByParity(int[] nums) {
-3        int j=0;
-4       for(int i =0;i<nums.length;i++){
-5            if(nums[i]%2==0){
-6                int temp =nums[i];
-7                nums[i]=nums[j];
-8                nums[j]=temp;
-9                j++;
-10            }
-11        }
-12    return nums;}
-13}
+2    public int search(int[] nums, int target) {
+3       int n = nums.length;
+4       int low = 0;
+5       int high = n - 1;
+6       
+7       while (low <= high) {
+8            int mid = (low + high) / 2;
+9            if (nums[mid] == target) {
+10                return mid;
+11            } else if (target < nums[mid]) {
+12                high = mid - 1;
+13            } else {
+14                low = mid + 1;
+15            }
+16       }
+17       return -1; 
+18    }
+19}
