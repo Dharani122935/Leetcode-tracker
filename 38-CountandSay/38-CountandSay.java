018861bf59e3451b893c56d9abe3376042d3b2cc
Dharani122935 <143819830+Dharani122935@users.noreply.github.com>
2026-07-09 18:11:03 +0530

    File updated at 09/07/2026, 18:11:00

--- 38-CountandSay/38-CountandSay.java
+++ 38-CountandSay/38-CountandSay.java
@@ -1,19 +1,15 @@
-// Last updated: 09/07/2026, 18:10:15
+// Last updated: 09/07/2026, 18:11:00
 1class Solution {
-2    public int findMaxConsecutiveOnes(int[] nums) {
-3        int k=0;
-4        int max=0;
-5        
-6        for (int i=0;i<nums.length;i++){
-7            if (nums[i]==1){
-8                k++;
-9            }
-10            else{
-11                max = Math.max(max,k);
-12                k=0;
-13            }
-14        }
-15        max =Math.max(k,max);
-16        return max;
-17    }
-18}
+2    public int findContentChildren(int[] g, int[] s) {
+3        Arrays.sort(g);
+4        Arrays.sort(s);
+5        int n = g.length, m = s.length, i = 0, j = 0;
+6
+7        while (i < m && j < n) {
+8            if (g[j] <= s[i]) j++;  
+9            i++;  
+10        }
+11
+12        return j;
+13    }
+14}
