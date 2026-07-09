7e7f4d0d8f47ea1c28aac448a6fb548bbe31120e
Dharani122935 <143819830+Dharani122935@users.noreply.github.com>
2026-07-09 22:03:30 +0530

    File updated at 09/07/2026, 22:03:27

--- 168-ExcelSheetColumnTitle/168-ExcelSheetColumnTitle.java
+++ 168-ExcelSheetColumnTitle/168-ExcelSheetColumnTitle.java
@@ -1,13 +1,8 @@
-// Last updated: 09/07/2026, 22:02:43
-1public class Solution {
-2    public boolean isPowerOfTwo(int n) {
-3        if (n == 0) return false;
-4        
-5        while (n > 0) {
-6            if (n == 1) return true;
-7            if (n % 2 != 0) break;
-8            n /= 2;
-9        }
-10        return false;
-11    }
-12}
+// Last updated: 09/07/2026, 22:03:27
+1class Solution {
+2    public boolean isPowerOfThree(int n) {
+3        if (n == 1) return true;
+4        if (n <= 0 || n % 3 != 0) return false;
+5        return isPowerOfThree(n / 3);
+6    }
+7}
