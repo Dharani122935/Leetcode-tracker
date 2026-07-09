dc319c3d7e917169fcf45162a4b672a92f625a9a
Dharani122935 <143819830+Dharani122935@users.noreply.github.com>
2026-07-09 18:28:47 +0530

    File updated at 09/07/2026, 18:28:43

--- 38-CountandSay/38-CountandSay.java
+++ 38-CountandSay/38-CountandSay.java
@@ -1,7 +1,15 @@
-// Last updated: 09/07/2026, 18:27:52
-1import java.time.LocalDate;
-2class Solution {
-3    public int dayOfYear(String date) {
-4        return LocalDate.parse(date).getDayOfYear();
-5    }
-6}
+// Last updated: 09/07/2026, 18:28:43
+1class Solution {
+2    public int tribonacci(int n) {
+3        int [] A = new int[n+1];
+4        if(n == 0 || n ==1) return n;
+5        if(n==2) return 1;
+6        A[0] = 0;
+7        A[1] = 1;
+8        A[2] = 1;
+9        for(int i = 3;i<=n;i++){
+10            A[i] = A[i-1]+A[i-2]+A[i-3];
+11        }
+12        return A[n];
+13    }
+14}
