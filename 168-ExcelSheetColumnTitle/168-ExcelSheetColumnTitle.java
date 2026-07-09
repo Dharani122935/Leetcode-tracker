81457d0431340ca78a3856619c8e5e25f6115aef
Dharani122935 <143819830+Dharani122935@users.noreply.github.com>
2026-07-09 22:22:54 +0530

    File updated at 09/07/2026, 22:22:51

--- 168-ExcelSheetColumnTitle/168-ExcelSheetColumnTitle.java
+++ 168-ExcelSheetColumnTitle/168-ExcelSheetColumnTitle.java
@@ -1,8 +1,20 @@
-// Last updated: 09/07/2026, 22:21:49
+// Last updated: 09/07/2026, 22:22:51
 1
 2class Solution {
-3    public void deleteNode(ListNode node) {
-4        node.val=node.next.val;
-5        node.next=node.next.next;
-6    }
-7}
+3    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+4
+5        if(list1!=null && list2!=null){
+6        if(list1.val<list2.val){
+7            list1.next=mergeTwoLists(list1.next,list2);
+8            return list1;
+9            }
+10            else{
+11                list2.next=mergeTwoLists(list1,list2.next);
+12                return list2;
+13        }
+14        }
+15        if(list1==null)
+16            return list2;
+17        return list1;
+18    }
+19}
