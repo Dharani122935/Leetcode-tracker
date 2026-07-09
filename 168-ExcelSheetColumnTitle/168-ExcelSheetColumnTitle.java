1ff6b45c6df5569f4c992c17eabfcf8589d03df8
Dharani122935 <143819830+Dharani122935@users.noreply.github.com>
2026-07-09 22:10:54 +0530

    File updated at 09/07/2026, 22:10:51

--- 168-ExcelSheetColumnTitle/168-ExcelSheetColumnTitle.java
+++ 168-ExcelSheetColumnTitle/168-ExcelSheetColumnTitle.java
@@ -1,34 +1,20 @@
-// Last updated: 09/07/2026, 22:09:28
+// Last updated: 09/07/2026, 22:10:51
 1class Solution {
-2    public int[] sortArray(int[] nums) {
-3        quick(nums, 0, nums.length - 1);
-4        return nums;
-5    }
-6
-7    static void quick(int[] nums, int low, int high) {
-8        if (low >= high) return;
-9
-10        int i = low;
-11        int j = high;
-12        int mid = (i + j) / 2;
-13        int pivot = nums[mid]; 
-14        while (i <= j) {
-15            while (nums[i] < pivot) {
-16                i++;
-17            }
-18            while (nums[j] > pivot) {
-19                j--;
-20            }
-21
-22            if (i <= j) {
-23                int temp = nums[i];
-24                nums[i] = nums[j];
-25                nums[j] = temp;
-26                i++;
-27                j--;
-28            }
-29        }
-30        quick(nums, low, j);
-31        quick(nums, i, high);
-32    }
-33}
+2    void dfs(int[][] img,int r,int c,int clr,int oc){
+3        if(r<0|| c<0 || r>=img.length||c>=img[0].length||img[r][c]!=oc){
+4            return;
+5        }
+6        img[r][c]=clr;
+7        dfs(img,r-1,c,clr,oc);
+8        dfs(img,r,c+1,clr,oc);
+9        dfs(img,r+1,c,clr,oc);
+10        dfs(img,r,c-1,clr,oc);
+11    }
+12    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+13        int oldCol=image[sr][sc];
+14        if(image[sr][sc]!=color){
+15            dfs(image,sr,sc,color,oldCol);
+16        }
+17        return image;
+18    }
+19}
