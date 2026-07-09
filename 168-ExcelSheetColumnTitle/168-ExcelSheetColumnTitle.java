b50cf39ce8cad4845b24c8ef2cabb709dfb09f15
Dharani122935 <143819830+Dharani122935@users.noreply.github.com>
2026-07-09 22:13:31 +0530

    File updated at 09/07/2026, 22:13:27

--- 168-ExcelSheetColumnTitle/168-ExcelSheetColumnTitle.java
+++ 168-ExcelSheetColumnTitle/168-ExcelSheetColumnTitle.java
@@ -1,36 +1,31 @@
-// Last updated: 09/07/2026, 22:12:25
+// Last updated: 09/07/2026, 22:13:27
 1class Solution {
-2    public String[] findWords(String[] words) {
-3        ArrayList<String> result = new ArrayList<>();
-4        for (String word : words) {
-5            if (keyBoardVerify(word)) {
-6                result.add(word);
-7            }
-8        }
-9        return result.toArray(new String[0]);
-10    }
-11    static boolean keyBoardVerify(String st) {
-12        String[] stArr = {
-13            "qwertyuiopQWERTYUIOP",
-14            "asdfghjklASDFGHJKL",
-15            "zxcvbnmZXCVBNM"
-16        };
-17        char ch = st.charAt(0);
-18        int ind = -1;
-19        // Find the row of the first letter
-20        for (int i = 0; i < stArr.length; i++) {
-21            if (stArr[i].indexOf(ch) >= 0) {
-22                ind = i;
-23                break;
-24            }
-25        }
-26        // Check if all characters are in that row
-27        for (int i = 1; i < st.length(); i++) {
-28            ch = st.charAt(i);
-29            if (stArr[ind].indexOf(ch) < 0) {
-30                return false;
-31            }
-32        }
-33        return true;
-34    }
-35}
+2    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+3        int m=nums1.length;
+4        int n=nums2.length;
+5        int p=m+n;
+6        int m1=(p-1)/2;
+7        int m2=p/2;
+8
+9        int i=0,j=0,count=0;
+10        int current=0,previous=0;
+11
+12        while(count<=m2){
+13            previous=current;
+14            if(i<m&&(j>=n||nums1[i]<=nums2[j])){
+15                current=nums1[i++];
+16            }
+17            else{
+18                current=nums2[j++];
+19            }
+20            count++;
+21        }
+22        if(p%2==0){
+23            return(previous+current)/2.0;
+24        }
+25        else{
+26            return current;
+27        }
+28
+29    }
+30}
