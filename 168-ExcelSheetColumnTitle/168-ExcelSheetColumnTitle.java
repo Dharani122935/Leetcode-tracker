eb63f606fde34b2f659bde9fcb903aab261dd6bc
Dharani122935 <143819830+Dharani122935@users.noreply.github.com>
2026-07-09 22:21:52 +0530

    File updated at 09/07/2026, 22:21:49

--- 168-ExcelSheetColumnTitle/168-ExcelSheetColumnTitle.java
+++ 168-ExcelSheetColumnTitle/168-ExcelSheetColumnTitle.java
@@ -1,17 +1,8 @@
-// Last updated: 09/07/2026, 22:19:09
+// Last updated: 09/07/2026, 22:21:49
 1
 2class Solution {
-3    public ListNode reverseList(ListNode head) {
-4        ListNode prev=null;
-5        ListNode current=head;
-6        while(current!=null){
-7            ListNode next=current.next;
-8            current.next=prev;
-9            prev=current;
-10            current=next;
-11        }
-12        return prev;
-13
-14
-15    }
-16}
+3    public void deleteNode(ListNode node) {
+4        node.val=node.next.val;
+5        node.next=node.next.next;
+6    }
+7}
