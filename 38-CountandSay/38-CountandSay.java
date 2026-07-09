ee4c72a355213c5d21fb6f9821ccf00bb0c2a62a
Dharani122935 <143819830+Dharani122935@users.noreply.github.com>
2026-07-09 18:14:08 +0530

    File updated at 09/07/2026, 18:14:04

--- 38-CountandSay/38-CountandSay.java
+++ 38-CountandSay/38-CountandSay.java
@@ -1,14 +1,25 @@
-// Last updated: 09/07/2026, 18:13:10
+// Last updated: 09/07/2026, 18:14:04
 1class Solution {
-2    public void moveZeroes(int[] nums) {
-3        int j = 0; 
-4        for (int i = 0; i < nums.length; i++) {
-5            if (nums[i] != 0) { 
-6                int temp = nums[i];
-7                nums[i] = nums[j];
-8                nums[j] = temp;
-9                j++;  
-10            }
-11        }
-12    }
-13}
+2    public int missingInteger(int[] nums) {
+3       int sum = nums[0];
+4        for (int i = 1; i < nums.length; i++) 
+5        {
+6            if (nums[i] == nums[i - 1] + 1) 
+7            {
+8                sum =sum+ nums[i];
+9            } else 
+10            {
+11                break; 
+12            }
+13        }
+14
+15        for (int i = 0; i < nums.length; i++) {
+16            if (nums[i] == sum) {
+17                sum++;  
+18                i = -1; 
+19            }
+20        }
+21
+22        return sum;
+23    }
+24}
