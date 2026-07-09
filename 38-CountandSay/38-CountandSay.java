a0de546fc8bcdb77c389de0a1df41fa4da40f6ac
Dharani122935 <143819830+Dharani122935@users.noreply.github.com>
2026-07-09 18:03:34 +0530

    File updated at 09/07/2026, 18:03:29

--- 38-CountandSay/38-CountandSay.java
+++ 38-CountandSay/38-CountandSay.java
@@ -1,20 +1,14 @@
-// Last updated: 09/07/2026, 18:02:43
+// Last updated: 09/07/2026, 18:03:29
 1class Solution {
-2    public int[] sortArrayByParityII(int[] nums) {
-3        int even = 0, odd = 1;
-4        int n = nums.length;
-5
-6        while (even < n && odd < n) {
-7            if (nums[even] % 2 == 1) {
-8                int temp = nums[even];
-9                nums[even] = nums[odd];
-10                nums[odd] = temp;
-11                odd += 2;
-12            } else {
-13                even += 2;
-14            }
-15
-16        }
-17            return nums;
-18    }
-19}
+2    public int[] sortArrayByParity(int[] nums) {
+3        int j=0;
+4       for(int i =0;i<nums.length;i++){
+5            if(nums[i]%2==0){
+6                int temp =nums[i];
+7                nums[i]=nums[j];
+8                nums[j]=temp;
+9                j++;
+10            }
+11        }
+12    return nums;}
+13}
