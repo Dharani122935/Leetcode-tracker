9f07be1d7ec705373e8102697c0bcc99fdc3394e
Dharani122935 <143819830+Dharani122935@users.noreply.github.com>
2026-07-09 18:29:39 +0530

    File updated at 09/07/2026, 18:29:36

--- 38-CountandSay/38-CountandSay.java
+++ 38-CountandSay/38-CountandSay.java
@@ -1,15 +1,20 @@
-// Last updated: 09/07/2026, 18:28:43
+// Last updated: 09/07/2026, 18:29:36
 1class Solution {
-2    public int tribonacci(int n) {
-3        int [] A = new int[n+1];
-4        if(n == 0 || n ==1) return n;
-5        if(n==2) return 1;
-6        A[0] = 0;
-7        A[1] = 1;
-8        A[2] = 1;
-9        for(int i = 3;i<=n;i++){
-10            A[i] = A[i-1]+A[i-2]+A[i-3];
-11        }
-12        return A[n];
-13    }
-14}
+2    public boolean isUgly(int n) {
+3        if(n <= 0) return false;
+4
+5        n = uglyChecker(n, 2);
+6        n = uglyChecker(n, 3);
+7        n = uglyChecker(n, 5);
+8
+9        return n == 1;
+10    }
+11
+12    public int uglyChecker(int num, int factor){
+13        while(num % factor == 0){
+14            num /= factor;
+15        }
+16
+17        return num;
+18    }
+19}
