78ed96e72ce2eb33ffb114c67c9a29f88c9001b1
Dharani122935 <143819830+Dharani122935@users.noreply.github.com>
2026-07-09 18:24:18 +0530

    File updated at 09/07/2026, 18:24:14

--- 38-CountandSay/38-CountandSay.java
+++ 38-CountandSay/38-CountandSay.java
@@ -1,20 +1,10 @@
-// Last updated: 09/07/2026, 18:21:26
+// Last updated: 09/07/2026, 18:24:14
 1class Solution {
-2    public boolean uniqueOccurrences(int[] arr) {
-3        Arrays.sort(arr);
-4        Set<Integer> st = new HashSet<>();
-5        int i = 0 , cnt = 0;
-6
-7        while(i < arr.length) {
-8            int j = i;
-9            while(j + 1 < arr.length && arr[j + 1] == arr[j]) {
-10                j++;
-11            }
-12            int freq = j - i + 1;
-13            st.add(freq);
-14            cnt++;
-15            i = j + 1;
-16        }
-17        return st.size() == cnt;
-18    }
-19}
+2    public int[] sortedSquares(int[] nums) {
+3        for(int i=0;i<nums.length;i++){
+4            nums[i]=nums[i]*nums[i];
+5        }
+6        Arrays.sort(nums);
+7        return nums;
+8    }
+9}
