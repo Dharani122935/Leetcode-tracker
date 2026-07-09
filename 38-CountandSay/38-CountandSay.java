4ca190b887c554f828a3496b0421f7ecb2eddaa8
Dharani122935 <143819830+Dharani122935@users.noreply.github.com>
2026-07-09 18:27:11 +0530

    File updated at 09/07/2026, 18:27:07

--- 38-CountandSay/38-CountandSay.java
+++ 38-CountandSay/38-CountandSay.java
@@ -1,14 +1,24 @@
-// Last updated: 09/07/2026, 18:26:20
+// Last updated: 09/07/2026, 18:27:07
 1class Solution {
-2    public boolean checkStraightLine(int[][] coordinates) {
-3        int x0=coordinates[0][0],y0=coordinates[0][1];
-4        int x1=coordinates[1][0],y1=coordinates[1][1];
-5        for(int i=2;i<coordinates.length;i++){
-6            int x2=coordinates[i][0],y2=coordinates[i][1];
-7            if((y1-y0)*(x2-x1)!=(y2-y1)*(x1-x0)){
-8                return false;
-9            }
-10        }
-11        return true;
-12    }
-13}
+2    public int countPrimes(int n) {
+3    int count=0;
+4     boolean[] prime=new boolean[n+1];
+5     for(int i=0;i<n;i++){
+6        prime[i]=true;
+7     }
+8     for(int p=2;p*p<=n;p++){
+9        if(prime[p]){
+10            for(int i=p*p;i<=n;i+=p){
+11                prime[i]=false;
+12            }
+13        }
+14     }
+15     for(int p=2;p<=n;p++){
+16        if(prime[p]){
+17            count++;
+18        }
+19     }
+20     return count;
+21
+22    }
+23}
