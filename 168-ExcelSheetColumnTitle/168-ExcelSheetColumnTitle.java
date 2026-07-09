1b976fb28b8dd766300e14815924bd5659e90599
Dharani122935 <143819830+Dharani122935@users.noreply.github.com>
2026-07-09 22:15:45 +0530

    File updated at 09/07/2026, 22:15:41

--- 168-ExcelSheetColumnTitle/168-ExcelSheetColumnTitle.java
+++ 168-ExcelSheetColumnTitle/168-ExcelSheetColumnTitle.java
@@ -1,17 +1,13 @@
-// Last updated: 09/07/2026, 22:14:32
+// Last updated: 09/07/2026, 22:15:41
 1
 2class Solution {
-3    List<Integer> li=new ArrayList<>();
-4    void preOrder(TreeNode root){
-5        if(root!=null){
-6            li.add(root.val);
-7            preOrder(root.left);
-8            preOrder(root.right);
-9        }
-10    }
-11    public List<Integer> preorderTraversal(TreeNode root) {
-12        preOrder(root);
-13        return li;
-14     
-15    }
-16}
+3    public List<Integer> inorderTraversal(TreeNode root) {
+4        List<Integer> list = new ArrayList<>();
+5        if(root != null){
+6            list.addAll(inorderTraversal(root.left));
+7            list.add(root.val);
+8            list.addAll(inorderTraversal(root.right));
+9            }
+10            return list;
+11    }
+12}
