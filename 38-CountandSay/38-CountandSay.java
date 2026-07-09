39d1bd138657e5d069e65a1c8bd01934eb142989
Dharani122935 <143819830+Dharani122935@users.noreply.github.com>
2026-07-09 18:31:11 +0530

    File updated at 09/07/2026, 18:31:07

--- 38-CountandSay/38-CountandSay.java
+++ 38-CountandSay/38-CountandSay.java
@@ -1,20 +1,10 @@
-// Last updated: 09/07/2026, 18:30:27
+// Last updated: 09/07/2026, 18:31:07
 1class Solution {
-2    public int addDigits(int num) {
-3        int sum=0;
-4        if(num==0){
-5            return 0;
+2    public int titleToNumber(String columnTitle) {
+3        int result = 0;
+4        for (int i = 0; i < columnTitle.length(); i++) {
+5            result = result * 26 + (columnTitle.charAt(i) - 'A' + 1);
 6        }
-7        else{
-8            while(num>0){
-9                int d=num%10;
-10                sum=sum+d;
-11                num=num/10;
-12            }
-13        }
-14        if(sum>=10){
-15            return addDigits(sum);
-16             }
-17         return sum;
-18    }
-19}
+7        return result;
+8    }
+9}
