a75ac66e53061be472facb1fd25935e3b7061d18
Dharani122935 <143819830+Dharani122935@users.noreply.github.com>
2026-07-09 22:09:31 +0530

    File updated at 09/07/2026, 22:09:28

--- 168-ExcelSheetColumnTitle/168-ExcelSheetColumnTitle.java
+++ 168-ExcelSheetColumnTitle/168-ExcelSheetColumnTitle.java
@@ -1,20 +1,34 @@
-// Last updated: 09/07/2026, 22:07:41
+// Last updated: 09/07/2026, 22:09:28
 1class Solution {
-2    public int climbStairs(int n) {
-3        if (n == 1) return 1;
-4        if (n == 2) return 2;
-5
-6        int first = 1; 
-7        int second = 2;
-8        int result = 0;
+2    public int[] sortArray(int[] nums) {
+3        quick(nums, 0, nums.length - 1);
+4        return nums;
+5    }
+6
+7    static void quick(int[] nums, int low, int high) {
+8        if (low >= high) return;
 9
-10        for (int i = 3; i <= n; i++) {
-11            result = first + second;
-12            first = second;
-13            second = result;
-14        }
-15
-16        return result;
-17    }
-18}
-19
+10        int i = low;
+11        int j = high;
+12        int mid = (i + j) / 2;
+13        int pivot = nums[mid]; 
+14        while (i <= j) {
+15            while (nums[i] < pivot) {
+16                i++;
+17            }
+18            while (nums[j] > pivot) {
+19                j--;
+20            }
+21
+22            if (i <= j) {
+23                int temp = nums[i];
+24                nums[i] = nums[j];
+25                nums[j] = temp;
+26                i++;
+27                j--;
+28            }
+29        }
+30        quick(nums, low, j);
+31        quick(nums, i, high);
+32    }
+33}
