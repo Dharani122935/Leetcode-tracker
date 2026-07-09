22389452ab3fb6605c18a547eb6e28dc7317a6fe
Dharani122935 <143819830+Dharani122935@users.noreply.github.com>
2026-07-09 18:16:26 +0530

    File updated at 09/07/2026, 18:16:22

--- 38-CountandSay/38-CountandSay.java
+++ 38-CountandSay/38-CountandSay.java
@@ -1,25 +1,24 @@
-// Last updated: 09/07/2026, 18:14:04
-1class Solution {
-2    public int missingInteger(int[] nums) {
-3       int sum = nums[0];
-4        for (int i = 1; i < nums.length; i++) 
-5        {
-6            if (nums[i] == nums[i - 1] + 1) 
-7            {
-8                sum =sum+ nums[i];
-9            } else 
-10            {
-11                break; 
-12            }
-13        }
-14
-15        for (int i = 0; i < nums.length; i++) {
-16            if (nums[i] == sum) {
-17                sum++;  
-18                i = -1; 
-19            }
-20        }
-21
-22        return sum;
-23    }
-24}
+// Last updated: 09/07/2026, 18:16:22
+1import java.util.*;
+2class Solution {
+3    public int maxFrequencyElements(int[] nums) {
+4        int[] freq = new int[101];  // numbers are in range 1–100
+5        for (int num : nums) {
+6            freq[num]++;
+7        }
+8
+9        int maxFreq = 0;
+10        for (int f : freq) {
+11            maxFreq = Math.max(maxFreq, f);
+12        }
+13
+14        int result = 0;
+15        for (int f : freq) {
+16            if (f == maxFreq) {
+17                result += f;
+18            }
+19        }
+20
+21        return result;
+22    }
+23}
