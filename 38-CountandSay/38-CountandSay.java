d54e7f4f63cd81b75ac73331a698201f14b75de3
Dharani122935 <143819830+Dharani122935@users.noreply.github.com>
2026-07-09 18:02:46 +0530

    File updated at 09/07/2026, 18:02:43

--- 38-CountandSay/38-CountandSay.java
+++ 38-CountandSay/38-CountandSay.java
@@ -1,33 +1,20 @@
-// Last updated: 09/07/2026, 18:01:49
-1import java.util.ArrayList;
-2import java.util.List;
-3
-4class Solution {
-5    public List<List<Integer>> generate(int numRows) {
-6        List<List<Integer>> result = new ArrayList<>();
-7        if (numRows == 0) {
-8            return result;
-9        }
-10
-11        if (numRows == 1) {
-12            List<Integer> firstRow = new ArrayList<>();
-13            firstRow.add(1);
-14            result.add(firstRow);
-15            return result;
+// Last updated: 09/07/2026, 18:02:43
+1class Solution {
+2    public int[] sortArrayByParityII(int[] nums) {
+3        int even = 0, odd = 1;
+4        int n = nums.length;
+5
+6        while (even < n && odd < n) {
+7            if (nums[even] % 2 == 1) {
+8                int temp = nums[even];
+9                nums[even] = nums[odd];
+10                nums[odd] = temp;
+11                odd += 2;
+12            } else {
+13                even += 2;
+14            }
+15
 16        }
-17
-18        result = generate(numRows - 1);
-19        List<Integer> prevRow = result.get(numRows - 2);
-20        List<Integer> currentRow = new ArrayList<>();
-21        currentRow.add(1);
-22
-23        for (int i = 1; i < numRows - 1; i++) {
-24            currentRow.add(prevRow.get(i - 1) + prevRow.get(i));
-25        }
-26
-27        currentRow.add(1);
-28        result.add(currentRow);
-29
-30        return result;
-31    }
-32}
+17            return nums;
+18    }
+19}
