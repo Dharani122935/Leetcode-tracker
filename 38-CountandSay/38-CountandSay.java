77461fda79ef628d07871ed8481487d481aa03a6
Dharani122935 <143819830+Dharani122935@users.noreply.github.com>
2026-07-09 18:01:53 +0530

    File updated at 09/07/2026, 18:01:49

--- 38-CountandSay/38-CountandSay.java
+++ 38-CountandSay/38-CountandSay.java
@@ -1,21 +1,33 @@
-// Last updated: 09/07/2026, 18:00:57
-1class Solution {
-2    public int areaOfMaxDiagonal(int[][] dimensions) {
-3        double maxi = -1.0;
-4        int res = 0;
-5        
-6        for (int[] rect : dimensions) {
-7            int l = rect[0], b = rect[1];
-8            double d = Math.sqrt((double)l * l + (double)b * b);
-9            
-10            if (d > maxi) {
-11                maxi = d;
-12                res = l * b;
-13            } else if (d == maxi) {
-14                res = Math.max(res, l * b);
-15            }
+// Last updated: 09/07/2026, 18:01:49
+1import java.util.ArrayList;
+2import java.util.List;
+3
+4class Solution {
+5    public List<List<Integer>> generate(int numRows) {
+6        List<List<Integer>> result = new ArrayList<>();
+7        if (numRows == 0) {
+8            return result;
+9        }
+10
+11        if (numRows == 1) {
+12            List<Integer> firstRow = new ArrayList<>();
+13            firstRow.add(1);
+14            result.add(firstRow);
+15            return result;
 16        }
-17        
-18        return res;
-19    }
-20}
+17
+18        result = generate(numRows - 1);
+19        List<Integer> prevRow = result.get(numRows - 2);
+20        List<Integer> currentRow = new ArrayList<>();
+21        currentRow.add(1);
+22
+23        for (int i = 1; i < numRows - 1; i++) {
+24            currentRow.add(prevRow.get(i - 1) + prevRow.get(i));
+25        }
+26
+27        currentRow.add(1);
+28        result.add(currentRow);
+29
+30        return result;
+31    }
+32}
