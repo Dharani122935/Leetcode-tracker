89e89528867357dea0face78c9a1ef5e64c0b75a
Dharani122935 <143819830+Dharani122935@users.noreply.github.com>
2026-07-09 18:21:30 +0530

    File updated at 09/07/2026, 18:21:26

--- 38-CountandSay/38-CountandSay.java
+++ 38-CountandSay/38-CountandSay.java
@@ -1,16 +1,20 @@
-// Last updated: 09/07/2026, 18:20:07
+// Last updated: 09/07/2026, 18:21:26
 1class Solution {
-2    public int searchInsert(int[] nums, int target) {
-3        int start = 0;
-4        int end = nums.length-1;
-5
-6        while (start <= end) {
-7            int mid = start + (end-start)/2;
-8            if (nums[mid] == target) return mid;
-9            else if (nums[mid] > target) end = mid-1;
-10            else start = mid+1;
-11        }
-12
-13        return start;
-14    }
-15}
+2    public boolean uniqueOccurrences(int[] arr) {
+3        Arrays.sort(arr);
+4        Set<Integer> st = new HashSet<>();
+5        int i = 0 , cnt = 0;
+6
+7        while(i < arr.length) {
+8            int j = i;
+9            while(j + 1 < arr.length && arr[j + 1] == arr[j]) {
+10                j++;
+11            }
+12            int freq = j - i + 1;
+13            st.add(freq);
+14            cnt++;
+15            i = j + 1;
+16        }
+17        return st.size() == cnt;
+18    }
+19}
