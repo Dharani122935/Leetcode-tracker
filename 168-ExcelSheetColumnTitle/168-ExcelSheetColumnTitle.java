12f1ea5d9eb7564c9d57a35e5769ac5bcca3098e
Dharani122935 <143819830+Dharani122935@users.noreply.github.com>
2026-07-09 22:24:34 +0530

    File updated at 09/07/2026, 22:24:30

--- 168-ExcelSheetColumnTitle/168-ExcelSheetColumnTitle.java
+++ 168-ExcelSheetColumnTitle/168-ExcelSheetColumnTitle.java
@@ -1,20 +1,12 @@
-// Last updated: 09/07/2026, 22:22:51
-1
-2class Solution {
-3    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-4
-5        if(list1!=null && list2!=null){
-6        if(list1.val<list2.val){
-7            list1.next=mergeTwoLists(list1.next,list2);
-8            return list1;
-9            }
-10            else{
-11                list2.next=mergeTwoLists(list1,list2.next);
-12                return list2;
-13        }
-14        }
-15        if(list1==null)
-16            return list2;
-17        return list1;
-18    }
-19}
+// Last updated: 09/07/2026, 22:24:30
+1class Solution {
+2    public boolean containsDuplicate(int[] nums) {
+3        Arrays.sort(nums);
+4        for(int i=0;i<nums.length-1;i++){
+5            if(nums[i]==nums[i+1]){
+6                return true;
+7            }
+8        }
+9        return false;
+10    }
+11}
