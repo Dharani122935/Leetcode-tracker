492e4954ab524fafa2ffcd3bbbdef2453ac8ec96
Dharani122935 <143819830+Dharani122935@users.noreply.github.com>
2026-07-13 10:12:03 +0530

    Create 217-ContainsDuplicate

---
+++ 217-ContainsDuplicate/217-ContainsDuplicate.java
@@ -0,0 +1,12 @@
+// Last updated: 13/07/2026, 10:11:55
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+                return true;
+            }
+        }
+        return false;
+    }
+}
