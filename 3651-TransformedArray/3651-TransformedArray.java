95924765e1750f8df7e4c890aebc3bd1559187d2
Dharani122935 <143819830+Dharani122935@users.noreply.github.com>
2026-09-02 09:49:34 +0530

    Create 3651-TransformedArray

---
+++ 3651-TransformedArray/3651-TransformedArray.java
@@ -0,0 +1,10 @@
+// Last updated: 02/09/2026, 09:48:38
+class Solution {
+    public int[] constructTransformedArray(int[] A) {
+        int n = A.length;
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++)
+            res[i] = A[(((i + A[i]) % n) + n) % n];
+        return res;
+    }
+}
