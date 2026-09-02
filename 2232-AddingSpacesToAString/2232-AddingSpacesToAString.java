a6eac87a724906414083ee94cfcd288f993044de
Dharani122935 <143819830+Dharani122935@users.noreply.github.com>
2026-09-02 09:50:05 +0530

    Create 2232-AddingSpacesToAString

---
+++ 2232-AddingSpacesToAString/2232-AddingSpacesToAString.java
@@ -0,0 +1,20 @@
+// Last updated: 02/09/2026, 09:49:09
+class Solution {
+    public String addSpaces(String s, int[] spaces) {
+        int n = s.length();
+        int m = spaces.length;
+        int idx = 0;
+        StringBuilder str = new StringBuilder();
+        for(int i=0;i<n;i++)
+        {
+            char ch = s.charAt(i);
+            if(idx<m && i==spaces[idx])
+            {
+                idx++;
+                str.append(" ");
+            }
+            str.append(ch);
+        }
+        return str.toString();
+    }
+}
