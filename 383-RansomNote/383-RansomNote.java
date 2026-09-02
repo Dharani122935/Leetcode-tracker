b87d08a0dd8ea3f7e844a938029cf13584b968f2
Dharani122935 <143819830+Dharani122935@users.noreply.github.com>
2026-09-02 09:54:09 +0530

    Create 383-RansomNote

---
+++ 383-RansomNote/383-RansomNote.java
@@ -0,0 +1,20 @@
+// Last updated: 02/09/2026, 09:53:13
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int r = ransomNote.length();
+        int m = magazine.length();
+        if (r > m) return false;
+        int[] mag = new int[26];
+        for (int i = 0; i < m; i++) {
+            mag[magazine.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < r; i++) {
+            char ch = ransomNote.charAt(i);
+            mag[ch - 'a']--;
+            if(mag[ch - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
