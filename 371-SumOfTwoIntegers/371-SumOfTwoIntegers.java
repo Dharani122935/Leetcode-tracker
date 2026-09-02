fc7a380a123c948b894290568251d98fef54247d
Dharani122935 <143819830+Dharani122935@users.noreply.github.com>
2026-09-02 09:54:27 +0530

    Create 371-SumOfTwoIntegers

---
+++ 371-SumOfTwoIntegers/371-SumOfTwoIntegers.java
@@ -0,0 +1,11 @@
+// Last updated: 02/09/2026, 09:53:31
+class Solution {
+    public int getSum(int a, int b) {
+        while (b != 0) {
+            int carry = (a & b) << 1;
+            a = a ^ b;
+            b = carry;
+        }
+        return a;
+    }
+}
