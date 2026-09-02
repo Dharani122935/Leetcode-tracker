8a86c781d0481b58fe76e0b8263e5c96931c681a
Dharani122935 <143819830+Dharani122935@users.noreply.github.com>
2026-09-02 09:58:01 +0530

    Create 137-SingleNumberIi

---
+++ 137-SingleNumberIi/137-SingleNumberIi.java
@@ -0,0 +1,18 @@
+// Last updated: 02/09/2026, 09:57:05
+class Solution {
+    public int singleNumber(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            boolean found = false;
+            for (int j = 0; j < nums.length; j++) {
+                if (i != j && nums[i] == nums[j]) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
