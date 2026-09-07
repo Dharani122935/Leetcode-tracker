80a8ecf4f3e7d9e9abe83ffa8e3fb2b79f0c85ee
Dharani122935 <143819830+Dharani122935@users.noreply.github.com>
2026-09-07 14:24:04 +0530

    File updated at 07/09/2026, 14:23:04

--- 3152-SpecialArrayII/3152-SpecialArrayII.java
+++ 3152-SpecialArrayII/3152-SpecialArrayII.java
@@ -1,21 +1,15 @@
-// Last updated: 07/09/2026, 14:10:52
+// Last updated: 07/09/2026, 14:23:04
 1class Solution {
-2    public boolean isTrionic(int[] nums) {
-3        int n = nums.length;
-4        if (n < 4) return false;
-5        int i = 0;
-6        while (i + 1 < n && nums[i] < nums[i+1]) {
-7            i++;
-8        }
-9        if (i == 0 || i == n - 1) return false;
-10        int p = i;
-11        while (i + 1 < n && nums[i] > nums[i+1]) {
-12            i++;
-13        }
-14        if (i == p || i == n - 1) return false;
-15        while (i + 1 < n && nums[i] < nums[i+1]) {
-16            i++;
-17        }
-18        return i == n - 1;
-19    }
-20}
+2    public int[] transformArray(int[] nums) {
+3        for(int i=0;i<nums.length;i++){
+4            if(nums[i]%2==0){
+5                nums[i]=0;         
+6            }
+7                else{
+8                    nums[i]=1;
+9                }
+10        }
+11        Arrays.sort(nums);
+12        return nums;
+13    }
+14}
