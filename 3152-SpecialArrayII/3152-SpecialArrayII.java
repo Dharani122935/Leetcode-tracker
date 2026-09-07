76f5e74c2ba1912ea9670e4c0e432c1bf60a3c04
Dharani122935 <143819830+Dharani122935@users.noreply.github.com>
2026-09-07 14:11:52 +0530

    File updated at 07/09/2026, 14:10:52

--- 3152-SpecialArrayII/3152-SpecialArrayII.java
+++ 3152-SpecialArrayII/3152-SpecialArrayII.java
@@ -1,19 +1,21 @@
-// Last updated: 07/09/2026, 13:57:55
+// Last updated: 07/09/2026, 14:10:52
 1class Solution {
-2    public boolean[] isArraySpecial(int[] nums, int[][] queries) {
-3        boolean[] ans = new boolean[queries.length];
-4        int[] ps = new int[nums.length];
-5        for(int i=1;i<nums.length;i++){
-6            ps[i] = ps[i-1];
-7            if(nums[i-1]%2 == nums[i]%2){
-8                ps[i]++;
-9            }
-10        }
-11        for(int i=0;i<queries.length;i++){
-12            int from = queries[i][0],to = queries[i][1];
-13            int cnt = ps[to] - ps[from];
-14            ans[i] = cnt>0 ? false : true;
-15        }
-16        return ans;
-17    }
-18}
+2    public boolean isTrionic(int[] nums) {
+3        int n = nums.length;
+4        if (n < 4) return false;
+5        int i = 0;
+6        while (i + 1 < n && nums[i] < nums[i+1]) {
+7            i++;
+8        }
+9        if (i == 0 || i == n - 1) return false;
+10        int p = i;
+11        while (i + 1 < n && nums[i] > nums[i+1]) {
+12            i++;
+13        }
+14        if (i == p || i == n - 1) return false;
+15        while (i + 1 < n && nums[i] < nums[i+1]) {
+16            i++;
+17        }
+18        return i == n - 1;
+19    }
+20}
