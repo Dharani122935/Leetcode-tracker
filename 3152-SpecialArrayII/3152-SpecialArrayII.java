8d0de878ffe267495e07a132680808c6f399b45f
Dharani122935 <143819830+Dharani122935@users.noreply.github.com>
2026-09-07 14:35:02 +0530

    File updated at 07/09/2026, 14:34:02

--- 3152-SpecialArrayII/3152-SpecialArrayII.java
+++ 3152-SpecialArrayII/3152-SpecialArrayII.java
@@ -1,15 +1,16 @@
-// Last updated: 07/09/2026, 14:23:04
+// Last updated: 07/09/2026, 14:34:02
 1class Solution {
-2    public int[] transformArray(int[] nums) {
-3        for(int i=0;i<nums.length;i++){
-4            if(nums[i]%2==0){
-5                nums[i]=0;         
-6            }
-7                else{
-8                    nums[i]=1;
-9                }
-10        }
-11        Arrays.sort(nums);
-12        return nums;
-13    }
-14}
+2    public List<String> stringMatching(String[] words) {
+3        List<String> result = new ArrayList<>();
+4        int n = words.length;
+5        for(int i = 0; i < n; i++) {
+6            for(int j = 0; j < n; j++) {
+7                if(i != j && words[j].contains(words[i])) {
+8                    result.add(words[i]);
+9                    break;
+10                }
+11            }
+12        }
+13        return result;
+14    }
+15}
